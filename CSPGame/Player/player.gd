88c8c781d39c6c0566e4587fb4b661ffc3264b45
Spookyinator <113743392+Spookyinator@users.noreply.github.com>
2023-10-02 12:50:58 -0700

    teehee

--- CSPGame/Player/player.gd
+++ CSPGame/Player/player.gd
@@ -2,13 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 75.0
-const slideSPEED = 300.0
+const slideSPEED = 175.0
 
-enum STATE {MOVE, SLIDE, SHOOTING}
+enum STATE {MOVE, SLIDE, SHOOT}
 
 var _state : int = STATE.MOVE
 
 var shooting = false
+
 var sliding = false
 
 func _physics_process(delta):
@@ -16,13 +17,13 @@ func _physics_process(delta):
 	var directiony = Input.get_action_strength("Down") - Input.get_action_strength("Up") 
 	
 	if Input.is_action_just_pressed("slide"):
-		pass
+		_state = STATE.SLIDE
+		$SlideTimer.start()
 	
 	if shooting and sliding == false:
 		_state = STATE.MOVE
 	
 	if _state == STATE.MOVE:
-
 		if directionx:
 			velocity.x = SPEED * directionx
 		else:
@@ -31,5 +32,13 @@ func _physics_process(delta):
 			velocity.y = SPEED * directiony
 		else:
 			velocity.y = lerp(SPEED, 0.0, 1.0)
+	
+	if _state == STATE.SLIDE:
+		if directionx:
+			velocity.x = slideSPEED * directionx
 		
 	move_and_slide()
+
+
+func _on_slide_timer_timeout():
+	_state = STATE.MOVE
