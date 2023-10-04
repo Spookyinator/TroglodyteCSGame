f1d9850d356fada9c0edd7a84ad8dc609db9db89
Spookyinator <113743392+Spookyinator@users.noreply.github.com>
2023-10-04 11:33:28 -0700

    state machine for enemy

--- CSPGame/Enemies/enemyTest.gd
+++ CSPGame/Enemies/enemyTest.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 40.0
-const ATKRANGE = 50.0
+const ATKRANGE = 20.0
+
+enum state {follow, attack}
+
 var player = null
 var canMove = true
+
 func _ready():
 	player = get_node("/root/Level/Player")
+	
 func target(targetPos):
 	#velocity = Vector2.ZERO
 	velocity = position.direction_to(player.position) * SPEED
@@ -14,15 +19,19 @@ func target(targetPos):
 	#print("Velocity: "+str(movement_vector))
 	#print("dist from player:"+str(distance))
 	move_and_slide()
+	
 func stop():
 	velocity = Vector2.ZERO
 	move_and_slide()
 
 func _physics_process(_delta):
+	var _state = state.follow
 	var distance = position.distance_to(player.global_position)
 	if distance<ATKRANGE:
 		stop()
+		_state = state.attack
 	else:
 		distance = position.distance_to(player.global_position)
 		target(player.global_position)
+		_state = state.follow
 		
