8b5d8df1f9e22461932dc39d149cca587e6bf895
Spookyinator <113743392+Spookyinator@users.noreply.github.com>
2023-10-06 12:25:38 -0700

    huhhh

--- CSPGame/World/level.gd
+++ CSPGame/World/level.gd
@@ -1,16 +1,6 @@
 extends Node2D
 @export var test_zombie_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_zombie_timer_timeout():
 	var zombie = test_zombie_scene.instantiate()
-	add_child(zombie)
+	add_child(zombie) 
