4176162c2dd60d17350cddf49c4c16e8b958880f
Spookyinator <113743392+Spookyinator@users.noreply.github.com>
2023-10-18 12:54:15 -0700

    Delete CSPGame directory

--- CSPGame/Screens/gameOver.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://Screens/menu.tscn")
-
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://World/level.tscn") # Replace with function body.
