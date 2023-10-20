1890e88677c2e3147c77e966874de6be9dedb998
Spookyinator <113743392+Spookyinator@users.noreply.github.com>
2023-10-20 12:14:38 -0700

    enemy movement bettered

--- CSPGame/Enemies/enemyTest.gd
+++ CSPGame/Enemies/enemyTest.gd
@@ -9,48 +9,25 @@ enum state {follow, attack}
 @onready var skin = $Skin
 
 var player = null
-#var canMove = true
+
 var gameOver = false
 
 signal isKilled
-func _ready():
-	if not gameOver:
-		player = get_node("/root/Level/Player")
-	else:
-		queue_free()
-		print("GAME OVER YEAHHHHH")
-func _handlePlayerDead():
-	gameOver = true
-func target(targetPos):
-	#velocity = Vector2.ZERO
-	velocity = position.direction_to(player.position) * SPEED
-	#var distance = position.distance_to(player.global_position)
-	#print("Velocity: "+str(movement_vector))
-	#print("dist from player:"+str(distance))
-	skin.look_at(targetPos)
-	move_and_slide()
+
+func _on_detection_body_entered(body):
+	player = body
 	
-func stop():
-	velocity = Vector2.ZERO
-	move_and_slide()
+func _on_detection_body_exited(body):
+	player = null
 
 func _physics_process(_delta):
-	var _state = state.follow
-	if not gameOver:
-		if (not player == null):
-			var distance = position.distance_to(player.global_position)
-			if distance<ATKRANGE:
-				stop()
-				_state = state.attack
-			else:
-				distance = position.distance_to(player.global_position)
-				target(player.global_position)
-				_state = state.follow
-	else:
-		print("GAME OVER YEAHHHHH")
-		queue_free()
-		
-		
+	if player != null:
+		var distance = position.distance_to(player.global_position)
+		if distance<ATKRANGE:
+			stop()
+		else:
+			distance = position.distance_to(player.global_position)
+			target(player.global_position)
 	update_health()
 
 func update_health():
@@ -67,13 +44,17 @@ func update_health():
 func _on_hitbox_no_health():
 	isKilled.emit(KILLPOINTS)
 	queue_free()
-	
-
-
-func _on_player_player_dead():
-	gameOver = true
-
 
 @warning_ignore("unused_parameter")
 func _on_zombie_hit(area):
 	isKilled.emit(HITPOINTS) # Replace with function body.
+
+
+func target(targetPos):
+	velocity = position.direction_to(targetPos) * SPEED
+	skin.look_at(targetPos)
+	move_and_slide()
+
+func stop():
+	velocity = Vector2.ZERO
+	move_and_slide()
