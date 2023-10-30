cdd17974562a9a9bccac51154ad391f7448320c7
Ishaan Saha <40528898+ishaaneagles@users.noreply.github.com>
2023-10-30 12:24:29 -0700

    grace period between waves

--- CSPGame/World/level.gd
+++ CSPGame/World/level.gd
@@ -3,14 +3,15 @@ extends Node2D
 @export var test_zombie_scene: PackedScene
 @export var power_up_scene: PackedScene
 @onready var _viewport = $CanvasLayer
-@onready var score = $CanvasLayer/ScoreLabel
-
+#@onready var score = $CanvasLayer/ScoreLabel
+@onready var grace_timer = $GraceTimer
 const POWER_UP_RATE = 0.5
 var isPlayerDead = false
 var gameOver = preload("res://Screens/gameOver.tscn")
 var points = 0
 var scoreLabel: Label
 var waveLabel: Label
+var waveAnnouncement: Label
 var canSpawn = true
 var waveNumber = 1
 
@@ -19,6 +20,7 @@ signal newWave
 func _ready():
 	scoreLabel = get_node("CanvasLayer/ScoreLabel")
 	waveLabel = get_node("CanvasLayer/WaveLabel")
+	waveAnnouncement = get_node("CanvasLayer/WaveAnnouncement")
 	update_score_label()
 	update_wave_label()
 func _physics_process(delta):
@@ -31,10 +33,11 @@ func _physics_process(delta):
 
 func on_new_wave():
 	newWave.emit()
-	canSpawn = true
+	grace_timer.start()
 	waveNumber += 1
 	totalZombies = waveNumber * 5
 	#print(waveNumber)
+	display_new_wave()	
 	update_wave_label()
 	
 func _on_zombie_timer_timeout():
@@ -56,14 +59,18 @@ func _on_zombie_killed(pointsScored, x, y):
 func spawn_power_up(x, y):
 	var powerup = power_up_scene.instantiate()
 	powerup.global_position = Vector2(x, y)
-	add_child(powerup);
+	add_child(powerup)
+#LABELS	
 func update_score_label():
 	scoreLabel.text = "Score: %d" % points
 func update_wave_label():
 	waveLabel.text = "Wave %d" % waveNumber
+func display_new_wave():
+	waveAnnouncement.text = "WAVE %d INCOMING..." % waveNumber
+	waveAnnouncement.visible = true
 func _on_player_player_dead():
 	var game_over_instance = gameOver.instantiate()
-	score.visible = false
+	scoreLabel.visible = false
 	_viewport.add_child(game_over_instance)
 	var gameMusic = get_node("GameMusic")
 	if gameMusic.is_playing():
@@ -71,3 +78,8 @@ func _on_player_player_dead():
 	isPlayerDead = true
 	var game_over = gameOver.instantiate()
 
+
+
+func _on_grace_timer_timeout():
+	canSpawn = true
+	waveAnnouncement.visible = false # Replace with function body.
