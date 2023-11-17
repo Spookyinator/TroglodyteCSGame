ce064c0aea076b63f0f3cc1980bb59cf88f53d3f
Ishaan Saha <40528898+ishaaneagles@users.noreply.github.com>
2023-11-17 12:30:16 -0800

    instakill powerups despawn after 10 seconds

--- CSPGame/World/level.gd
+++ CSPGame/World/level.gd
@@ -2,10 +2,10 @@ extends Node2D
 #zombie scene, power up scene
 @export var test_zombie_scene: PackedScene
 @export var power_up_scene: PackedScene
-@onready var _viewport = $CanvasLayers
+@onready var _viewport = $CanvasLayer
 @onready var camera = $Camera2D
 @onready var grace_timer = $GraceTimer
-const POWER_UP_RATE = 0.5
+const POWER_UP_RATE = 0.05
 var isPlayerDead = false
 var gameOver = preload("res://Screens/gameOver.tscn")
 var scoreLabel: Label
@@ -65,7 +65,7 @@ func spawn_power_up(x, y):
 	var powerup = power_up_scene.instantiate()
 	powerup.global_position = Vector2(x, y)
 	powerup.instakill_zombies.connect(_on_zombies_instakill)
-	add_child(powerup)
+	call_deferred("add_child",powerup)
 #LABELS	
 
 func _on_zombies_instakill():
