393aa6b4a9696f4908d6b5c660ecece36dd08329
DanielWantsToGoToIvys <123143269+DanielWantsToGoToIvys@users.noreply.github.com>
2023-11-19 16:32:53 -0800

    FIXED THE CAMERA!!!!!
    I FIXED THE CAMERA!!!!! need to implement in all levels

--- CSPGame/Hitboxes & Hurtboxes/camerachange.gd
+++ CSPGame/Hitboxes & Hurtboxes/camerachange.gd
@@ -3,16 +3,18 @@ var cameraA: Camera2D
 var cameraB: Camera2D
 
 func _ready():
-	#cameraA = $Camera2D
-	#cameraB = $Camera2D2
-	#cameraA.set_process(true)
+	cameraA = $"../Camera2D"
+	cameraB = $"../Camera2D2"
+	cameraA.set_process(true)
 	pass
 func _on_body_entered(body):
-	#switch_cameras()
+	switch_cameras()
 	print("entered")
 func switch_cameras():
-	#cameraA.set_process(false)
-	#cameraB.set_process(true)
-	pass
+	if cameraA.is_current():
+		cameraB.make_current()
+	else:
+		cameraA.make_current()
+
 func _on_body_exited(body):
 	print("exited")
