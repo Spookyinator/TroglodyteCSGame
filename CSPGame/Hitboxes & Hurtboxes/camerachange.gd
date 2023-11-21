287ec12f4a31c10fab8f17a79b9e9c956aa8e847
DanielWantsToGoToIvys <123143269+DanielWantsToGoToIvys@users.noreply.github.com>
2023-11-20 22:31:30 -0800

    made the camera much more robust
    stopped this really annoying glitch where if you didn't exit the body on the expected side, the whole thing would be incorrect. also fixed the camera not working if you barely moved before exiting the body.

--- CSPGame/Hitboxes & Hurtboxes/camerachange.gd
+++ CSPGame/Hitboxes & Hurtboxes/camerachange.gd
@@ -1,20 +1,48 @@
 extends Area2D
+
 var cameraA: Camera2D
 var cameraB: Camera2D
+var Player_previous_position: Vector2 = Vector2()
+var Player
 
 func _ready():
 	cameraA = $"../Camera2D"
 	cameraB = $"../Camera2D2"
 	cameraA.set_process(true)
-	pass
-func _on_body_entered(body):
-	switch_cameras()
-	print("entered")
-func switch_cameras():
-	if cameraA.is_current():
-		cameraB.make_current()
+	Player = $"../Player"
+	if is_instance_valid(Player):
+		Player_previous_position = Player.global_position
 	else:
-		cameraA.make_current()
+		print("Player node not found")
+	Player_previous_position = Vector2()
+	pass
+
+func _physics_process(delta):
+	if is_instance_valid(Player):
+			Player_previous_position = Player.global_position - Player.velocity * delta
+			Player.global_position += Player.velocity * delta
+
+func _on_body_entered(body):
+	if is_instance_valid(body) and body.name == "Player":  
+		handle_camera_switch(body)
+
+func handle_camera_switch(player):
+	if is_instance_valid(Player):
+		var Player_current_position = Player.global_position
+		var movement_direction = Player_current_position - Player_previous_position
+		if is_moving_towards_roomB(movement_direction):
+			if not cameraB.is_current():
+				cameraB.make_current()
+				print(movement_direction)
+		elif is_moving_towards_roomA(movement_direction):
+			if not cameraA.is_current():
+				cameraA.make_current()
+				print(movement_direction)
+func is_moving_towards_roomB(movement_direction: Vector2) -> bool:
+	return movement_direction.x > 0  # Moving right
+
+func is_moving_towards_roomA(movement_direction: Vector2) -> bool:
+	return movement_direction.x < 0  # Moving left
 
 func _on_body_exited(body):
-	print("exited")
+	handle_camera_switch(body)
