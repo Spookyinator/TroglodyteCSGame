9dcbd932d5f6ebf460ac5c59b0d645a83f2d14a5
Ishaan Saha <40528898+ishaaneagles@users.noreply.github.com>
2023-11-29 12:34:03 -0800

    fixed the parsing issues

--- CSPGame/Powerups/powerup.gd
+++ CSPGame/Powerups/powerup.gd
@@ -1,20 +1,16 @@
 extends Area2D
 
 @onready var animation_player = $AnimationPlayer
+#@onready var _animated_sprite = $AnimatedSprite2D
 signal powerup_consumed
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-<<<<<<< Updated upstream
-func _process(delta):
-	_animated_sprite.play("default")
-=======
 func _physics_process(delta):
 	animation_player.play("Idle")
->>>>>>> Stashed changes
 
 func on_powerup_consumed(powerup):
 	get_node("PlayerDetection").player.activate_powerup(powerup)
 	powerup_consumed.emit(powerup)
-	queue_free() # Replace with function body.
+	queue_free()
 
 
 func _on_despawn_timer_timeout():
