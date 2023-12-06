2d9dbd88f307b538e79c13f32a092834409a5fa5
Lucas <124012321+acoolusername255@users.noreply.github.com>
2023-12-06 11:55:28 -0800

    Fix powerups
    Bug was, if the player collects 2 powerups in a row without interacting with anything else, they don't work

--- CSPGame/Powerups/PlayerDetection.gd
+++ CSPGame/Powerups/PlayerDetection.gd
@@ -4,8 +4,9 @@ var player = null
 signal powerup_consumed
 func _on_body_entered(body):
 	# Add code to kill all the zombies here
+	var powerup = get_parent().get_child(0).name
 	player = body
-	powerup_consumed.emit(get_parent().name)
+	powerup_consumed.emit(powerup)
 
 func _on_body_exited(body):
 	player = null
